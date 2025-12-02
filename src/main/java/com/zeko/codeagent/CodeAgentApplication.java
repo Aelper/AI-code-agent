12b5370e3f0ea6899aa996c97856a736c1c13274
zeko <543653825@qq.com>
2025-12-02 23:07:36 +0800

    ai-code-agent [后端]完成用户模块开发

--- src/main/java/com/zeko/codeagent/CodeAgentApplication.java
+++ src/main/java/com/zeko/codeagent/CodeAgentApplication.java
@@ -1,10 +1,12 @@
 package com.zeko.codeagent;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.zeko.codeagent.mapper")
 @SpringBootApplication
 public class CodeAgentApplication {
 
---
+++ src/main/java/com/zeko/codeagent/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.zeko.codeagent.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.zeko.codeagent.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
